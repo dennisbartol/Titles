6d88d8322b89fa0182912f4d7274b20a46148933
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-01-31 12:33:34 +0100

    Update multiple.js

--- multiple.js
+++ multiple.js
@@ -1,2 +1,8 @@
 let titles = ["Welcome to my website !", "Home of cool and creative stuff", "Amazing content !"]; 
 let counter = 0; 
+
+setInterval(function() {
+    document.title = titles[counter % titleText.length];
+    counter++;
+
+}, 1500);
